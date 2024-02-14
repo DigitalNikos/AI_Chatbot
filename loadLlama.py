b15a539a70f5f070e756b49bf9bddd90b96bdbe9
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-02-14 12:17:52 +0100

    add loadLlama.py

---
+++ loadLlama.py
@@ -0,0 +1,33 @@
+from torch import cuda, bfloat16
+import transformers
+
+model_id = 'meta-llama/Llama-2-13b-chat-hf'
+
+# 4-bit Quantization to load Llama 2 with less GPU memory
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+# Llama 2 Tokenizer
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+
+# Llama 2 Model
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    quantization_config=bnb_config,
+    device_map='auto',
+)
+model.eval()
+
+# Our text generator
+generator = transformers.pipeline(
+    model=model, tokenizer=tokenizer,
+    task='text-generation',
+    temperature=0.1,
+    max_new_tokens=500,
+    repetition_penalty=1.1
+)
